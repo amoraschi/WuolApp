13bf889e9d0227aa395a6d2893d39e80a8a722b8
Angelo Moraschi <68395930+amoraschi@users.noreply.github.com>
2024-02-20 12:17:04 +0100

    Change path of config file

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -2,27 +2,27 @@ import { Config } from '@/types/Common'
 import { BaseDirectory, exists, readTextFile, writeTextFile } from '@tauri-apps/api/fs'
 
 export async function getConfig (): Promise<Config> {
-  const existsFile = await exists('config.json', {
+  const existsFile = await exists('wuolapp.json', {
     dir: BaseDirectory.AppData
   })
 
   if (!existsFile) {
-    await writeTextFile('config.json', '{}', {
-      dir: BaseDirectory.AppData
+    await writeTextFile('wuolapp.json', '{}', {
+      dir: BaseDirectory.LocalData
     })
   }
 
-  const config = await readTextFile('config.json', {
-    dir: BaseDirectory.AppData
+  const config = await readTextFile('wuolapp.json', {
+    dir: BaseDirectory.LocalData
   })
 
   return JSON.parse(config)
 }
 
 export async function setConfig (config: Config): Promise<void> {
-  console.log('Config:', 'AppData')
-  
-  await writeTextFile('config.json', JSON.stringify(config), {
-    dir: BaseDirectory.AppData
+  console.log('Config:', 'Local')
+
+  await writeTextFile('wuolapp.json', JSON.stringify(config), {
+    dir: BaseDirectory.LocalData
   })
 }
